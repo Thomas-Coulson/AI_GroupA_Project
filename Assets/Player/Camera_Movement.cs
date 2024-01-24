184df99a29870278ae37405edb8b08617cb92c1f
Thomas-Coulson <A0050289@live.tees.ac.uk>
2024-01-24 14:25:32 +0000

    Added basic camera movement

--- Assets/Player/Camera_Movement.cs
+++ Assets/Player/Camera_Movement.cs
@@ -9,6 +9,10 @@ public class Camera_Movement : MonoBehaviour
 
     public float xSpeed;
     public float ySpeed;
+    public float zoomSpeed;
+
+    Vector3 pos;
+    Vector3 lookDir;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,27 @@ public class Camera_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        calculateMovement();
+    }
+
+    void calculateMovement()
+    {
+        pos = transform.position;
+        lookDir = transform.forward;
+
+        pos.x += Input.GetAxis("Horizontal") * Time.deltaTime * xSpeed;
+        pos.z += Input.GetAxis("Vertical") * Time.deltaTime * ySpeed;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            pos += lookDir * Time.deltaTime * zoomSpeed;
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            pos += lookDir * Time.deltaTime * -zoomSpeed;
+        }
+
+        transform.position = pos;
 
     }
 }
