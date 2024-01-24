de5d52af8c777de2becd2da0fce40a9ca3655451
Thomas Coulson <78150031+Thomas-Coulson@users.noreply.github.com>
2024-01-24 13:40:03 +0000

    Added terrain and tweaked camera (#1)

---
+++ Assets/Player/Camera_Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{
+
+    public float xSpeed;
+    public float ySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
