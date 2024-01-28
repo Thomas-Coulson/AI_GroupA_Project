bdda3c8ea616315f4812517ef953b4ca908e7293
Thomas-Coulson <A0050289@live.tees.ac.uk>
2024-01-28 15:18:05 +0000

    Implemented AStar algorithm
    Implemented AStar Algorithm with visual representation
    - Currently has temp seeker and target objects to be replaced later
    - path is currently drawn with gizmos

--- Assets/Pathfinding/Node.cs
+++ Assets/Pathfinding/Node.cs
@@ -6,10 +6,26 @@ public class Node
 {
     public bool m_walkable;
     public Vector3 m_worldPos;
+    public int m_gridX, m_gridY;
 
-    public Node(bool walkable, Vector3 worldPos)
+    public Node m_parent;
+    public int m_gCost, m_hCost;
+
+    public int m_fCost
+    {
+        get
+        {
+            return m_gCost + m_hCost;
+        }
+    }
+
+    public Node(bool walkable, Vector3 worldPos, int gridX, int gridY)
     {
         m_walkable = walkable;
         m_worldPos = worldPos;
+        m_gridX = gridX;
+        m_gridY = gridY;
     }
+
+    
 }
